e555541e3f06d5f9445334afaa0646b97416c597
Rodrigo <rwebler@gmail.com>
2017-10-18 12:18:24 -0200

    Even/Odd exercise with fixes

--- evenodd/main.go
+++ evenodd/main.go
@@ -6,12 +6,10 @@ func main() {
 	// will research a way to do it without typing all elements
 	ints := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	for _, n := range ints {
-		// figuring out a way to define out took some minutes
-		// using else wouldn't work :)
-		out := fmt.Sprintf("%d is odd", n)
 		if n%2 == 0 {
-			out = fmt.Sprintf("%d is even", n)
+			fmt.Println(n, "is even")
+		} else {
+			fmt.Println(n, "is odd")
 		}
-		fmt.Println(out)
 	}
 }

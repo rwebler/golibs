12a70f059b3d4406565aac33b660bd8cf81411e6
Rodrigo <rwebler@gmail.com>
2017-10-24 12:55:56 -0200

    Valenciano Web Server

---
+++ valenciano/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	_, err := os.Open("public_html/index.html")
+	if err != nil {
+		t, err := template.ParseFiles("template/index.tmpl.html")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		data := struct {
+			PrecoSolteiro string
+			PrecoCasal    string
+		}{
+			PrecoSolteiro: "65",
+			PrecoCasal:    "110",
+		}
+		f, err := os.Create("public_html/index.html")
+		if err != nil {
+			fmt.Println(err)
+		}
+		t.Execute(f, data)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("In static file handler", r.URL.Path)
+		http.ServeFile(w, r, "public_html/"+r.URL.Path[1:])
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
